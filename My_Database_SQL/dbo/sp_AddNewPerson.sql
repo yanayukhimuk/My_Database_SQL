5ad6f83fe5288a4bc5e0618f7a773eb5ec229902
yanayukhimuk <yana.ryzhikova.brest.belarus@gmail.com>
2022-12-28 23:45:00 +0300

    Created Stored Procedures

---
+++ My_Database_SQL/dbo/sp_AddNewPerson.sql
@@ -0,0 +1,25 @@
+﻿CREATE PROCEDURE [dbo].[sp_AddNewPerson]
+	@EmployeeName nvarchar NULL,
+    @FirstName nvarchar (50) NULL,
+    @LastName nvarchar (50) NULL,
+    @CompanyName nvarchar (20) NOT NULL,
+    @Position nvarchar (100) NULL, 
+    @Street nvarchar (50) NOT NULL, 
+    @City nvarchar (50) NULL,
+    @State nvarchar (50) NULL, 
+    @ZipCode nvarchar (50) NULL
+AS
+BEGIN
+	SELECT [Id], [FirstName], [LastName]
+    From dbo.Person
+    Where FirstName = @FirstName AND LastName = @LastName;
+    SELECT [EmployeeName], [Position]
+    From dbo.Employee
+    Where EmployeeName = @EmployeeName and Position = @Position;
+    SELECT [Name]
+    From dbo.Company
+    Where Name = @CompanyName;
+    SELECT [Street], [City], [State], [ZipCode]
+    From dbo.Address
+    Where Street = @Street AND City = @City AND State = @State AND ZipCode = @ZipCode;
+END
